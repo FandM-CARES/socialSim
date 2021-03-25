31d6fef05496c3c40ec3cbc4e7c6338381e1611c
Dyani Dillard <ddillard@oxy.edu>
2021-03-25 16:17:59 -0700

    -

---
+++ my_staghunt.py
@@ -0,0 +1,21 @@
+from companionsKQML.pythonian import Pythonian
+import run_sim as sim
+import logging
+
+logger = logging.getLogger('StagHuntAgent')
+
+class StagHuntAgent(Pythonian):
+
+    name = "StagHuntAgent"
+
+    def __init__(self, **kwargs):
+        super(StagHuntAgent, self).__init__(**kwargs)
+        self.add_achieve(sim.my_make_game, 'my_make_game')
+        self.add_achieve(sim.my_run_one, 'my_run_one')
+
+
+if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
+    AGENT = StagHuntAgent.parse_command_line_args() # kqml AssertionError: Connection formed but output (None) not set.
+
+# (achieve :receiver StagHuntAgent :content (task :action (run_sim (2, 1, 3))))
