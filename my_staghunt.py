330d4883030738d4f0ed0b1865b1ffb41cee398f
Dyani Dillard <ddillard@oxy.edu>
2021-06-09 20:13:46 -0700

    insert into MT and added planning

--- my_staghunt.py
+++ my_staghunt.py
@@ -8,6 +8,7 @@ logger = logging.getLogger('StagHuntAgent')
 class StagHuntAgent(Pythonian):
 
     name = "StagHuntAgent"
+    state = None
 
     def __init__(self, **kwargs):
         super(StagHuntAgent, self).__init__(**kwargs)
@@ -18,17 +19,19 @@ class StagHuntAgent(Pythonian):
 
     @staticmethod
     def set_goal(hunter, coopWith):
-        state = run_sim.my_make_game(list(map(kqml.convert_to_int, (2, 1, 3))))
-        run_sim.my_assignGoals(state, ('h1', 'hunter'), ('h2', 'hunter'))
-        return run_sim.my_run_one(state, False)
+        if not StagHuntAgent.state:
+            logger.debug("no game made")
+        else:
+            run_sim.my_assignGoals(StagHuntAgent.state, ('h1', 'hunter'), ('h2', 'hunter'))
+            StagHuntAgent.state = run_sim.my_run_one(StagHuntAgent.state, False)
 
     @staticmethod
     def make_game(agents):
-        return run_sim.my_make_game(list(map(kqml.convert_to_int, agents)))
+         StagHuntAgent.state = run_sim.my_make_game(list(map(kqml.convert_to_int, agents)))
 
     @staticmethod
-    def run_one(state):
-        return run_sim.my_run_one(state, False)
+    def run_one():
+        StagHuntAgent.state = run_sim.my_run_one(StagHuntAgent.state, True)
 
     @staticmethod
     def run_sim(agents, n):
@@ -38,10 +41,11 @@ class StagHuntAgent(Pythonian):
 if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     AGENT = StagHuntAgent.parse_command_line_args()
-    AGENT.insert_to_microtheory("StagHuntAgent", "(distanceBetween hunter1 hunter2 2)", "GameOntologyMt")
+    AGENT.insert_to_microtheory("session-reasoner", "(distanceBetween hunter1 hunter3 2)", "GameOntologyMt")
 
 
 # (achieve :receiver StagHuntAgent :content (task :action (run_sim (2 1 3) 1)))
 
 # (achieve :receiver StagHuntAgent :content (task :action (make_game (2 1 3))))
-# (achieve :receiver StagHuntAgent :content (task :action (run_one (state??)))
+# (achieve :receiver StagHuntAgent :content (task :action (set_goal hunter1 hunter2)))
+# (achieve :receiver StagHuntAgent :content (task :action run_one))
