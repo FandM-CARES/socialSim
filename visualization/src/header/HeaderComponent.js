555b81f6812fee5b4c681ef41aae262981d87c8e
aamanhey <amanhey@oxy.edu>
2022-04-25 19:16:23 -0700

    Removed unnecessary header items

--- visualization/src/header/HeaderComponent.js
+++ visualization/src/header/HeaderComponent.js
@@ -8,9 +8,6 @@ class HeaderComponent extends React.Component {
           <div className="header">
             <a href="#default" className="logo">Staghunt</a>
             <div className="header-right">
-              <a className="active" href="#home">Home</a>
-              <a href="#contact">Contact</a>
-              <a href="#about">About</a>
             </div>
           </div>
         </div>
